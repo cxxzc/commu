a3a7816d6980c5939aa0975efb29122df1b7cbc8
wong <2105862811@qq.com>
2020-02-23 00:49:20 +0800

    add githubAccessToken and AuthorizeController

---
+++ src/main/java/project/wong/comm/controller/AuthorizeController.java
@@ -0,0 +1,31 @@
+package project.wong.comm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import project.wong.comm.dto.AccessTokenDTO;
+import project.wong.comm.dto.GithubUser;
+import project.wong.comm.provider.GithubProvider;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id("6e6ef5a619642b83de1b");
+        accessTokenDTO.setClient_secret("c525f608be8b77875fbbcd793eb48fb4f7a7458e");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
